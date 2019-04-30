f58c90b2e655d93bf0062d87c5ea07ff9d62db06
Jay Swanson <jrswanson020193@gmail.com>
2019-04-30 15:09:02 -0700

    Add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,37 @@
+import * as APIUtils from '../utils/session_api_util';
+
+export const RECEIVE_USER = 'RECEIVE_USER';
+export const LOGOUT = 'LOGOUT';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receieveCurrentUser = (user) => ({
+    type: RECEIVE_USER,
+    user
+});
+
+export const logoutCurrentUser = () => ({
+    type: LOGOUT
+});
+
+export const receieveErrors = (errors) => ({
+    type: RECEIVE_ERRORS,
+    errors
+});
+
+export const signup = user => dispatch => (
+    APIUtils.signup(user)
+        .then(res => dispatch(receieveCurrentUser(res)))
+        .fail(errors => dispatch(receieveErrors(errors)))
+);
+
+export const login = user => dispatch => (
+    APIUtils.login(user)
+        .then(res => dispatch(receieveCurrentUser(res)))
+        .fail(errors => dispatch(receieveErrors(errors)))
+);
+
+export const logout = () => dispatch => (
+    APIUtils.logout()
+        .then(() => dispatch(logoutCurrentUser()))
+        .fail(errors => dispatch(receieveErrors(errors)))
+);
