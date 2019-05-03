6d13631491a8904aac3d453d367eede863b329a0
Jay Swanson <jrswanson020193@gmail.com>
2019-05-03 10:23:09 -0700

    Add styling, file upload button

--- frontend/components/NewPictureForm.jsx
+++ frontend/components/NewPictureForm.jsx
@@ -6,12 +6,16 @@ class NewPictureForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { drag: 'drag-hide' };
+        this.state = {
+            drag: 'drag-hide',
+            photoFile: null
+        };
 
         this.closeModal = this.closeModal.bind(this);
         this.handleDragEnter = this.handleDragEnter.bind(this);
         this.handleDragLeave = this.handleDragLeave.bind(this);
         this.handleDrop = this.handleDrop.bind(this);
+        this.handleFile = this.handleFile.bind(this);
     }
 
     closeModal(e) {
@@ -32,22 +36,58 @@ class NewPictureForm extends React.Component {
         e.preventDefault();
         e.stopPropagation();
         this.setState({ drag: 'drag-hide' });
+        this.setState({ photoFile: e.dataTransfer.files[0] });
+    }
+
+    handleFile(e) {
+        e.preventDefault();
+        this.setState({ photoFile: e.currentTarget.files[0] });
     }
 
     render() {
-        return (
-            <>
-                <div className={this.state.drag}
-                    onDragOver={e => e.preventDefault()}
-                    onDragLeave={this.handleDragLeave}
-                    onDrop={this.handleDrop}></div>
-                <div className='modal-background'
-                    onDragEnter={this.handleDragEnter}
-                    onClick={this.closeModal}>
-                    <div className='modal-child' onClick={e => e.stopPropagation()}></div>
-                </div>
-            </>
-        );
+        if (this.state.photoFile) {
+            return (
+                <>
+                    <div className='modal-background'
+                        onClick={this.closeModal}>
+                        <div className='modal-child' onClick={e => e.stopPropagation()}>
+                            <form className='new-pic-form'>
+                                <label htmlFor='title'>Title</label>
+                                <input id='title' type='text' />
+                                <label htmlFor='description'>Description</label>
+                                <textarea
+                                    id='description'
+                                    placeholder="Tell us more about your beautiful photo">
+                                </textarea>
+                            </form>
+                        </div>
+                    </div>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <div className={this.state.drag}
+                        onDragOver={e => e.preventDefault()}
+                        onDragLeave={this.handleDragLeave}
+                        onDrop={this.handleDrop}></div>
+                    <div className='modal-background'
+                        onDragEnter={this.handleDragEnter}
+                        onClick={this.closeModal}>
+                        <div className='modal-child' onClick={e => e.stopPropagation()}>
+                            <div className='select-photo'>
+                                <label className='new-pic-button'>
+                                    Select Photo
+                                    <input type='file'
+                                        onChange={this.handleFile}/>
+                                </label>
+                                <p>Or drag & drop photos anywhere on this page</p>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            );
+        }
     }
 }
 
