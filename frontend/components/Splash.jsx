6dfc5f92a1743e438e340d555d7ac3167ba0f063
Jay Swanson <jrswanson020193@gmail.com>
2019-06-03 18:13:06 -0700

    Add more to splash page

--- frontend/components/Splash.jsx
+++ frontend/components/Splash.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default props => (
-    <div id='splash'>
-        <header>
+    <div className='splash'>
+        <div className='splash-nav'>
             <Link className='logo' to='/'>ShutterStop</Link>
             <div className='right-nav'>
                 <Link id='login-link-splash' to='/login'>Log in</Link>
                 <Link id='signup-link-splash' to='/signup'>Sign up</Link>
             </div>
-        </header>
-        <img src="./assets/wave.svg" alt="" id='wave'/>
-        <main>
-            <img src="./assets/splash.jpg" alt="" id='splash-img'/>
+        </div>
+        <img src="./assets/wave.svg" alt="" className='wave'/>
+        <div className='splash-white'></div>
+        <div className='splash-main'>
+            <img src="./assets/splash.jpg" alt="" className='splash-img'/>
             <div></div>
-        </main>
+        </div>
+        <div className='splash-footer'>
+            <ul>
+                <li><a href="https://github.com/jrswanson/" class="icon style2 fa-github"><span class="label">Github</span></a></li>
+                <li><a href="https://www.linkedin.com/in/j-r-swanson/" class="icon style2 fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                <li><a href="mailto:j.swanson.dev@gmail.com" class="icon style2 fa-envelope"><span class="label">Email</span></a></li>
+            </ul>
+        </div>
     </div>
 );
